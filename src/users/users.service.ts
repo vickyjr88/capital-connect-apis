c473f1ac48828910db9f515cd9169aaf1ca1bef6
Reuben Bichage <rbichage@oldmutual.com>
2024-05-30 19:08:07 +0300

    Validate username to make sure it is a valid email address.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
@@ -29,5 +30,29 @@ export class UsersService {
 
   async findAll(): Promise<User[]> {
     return this.usersRepository.find();
+  }
+
+  async update(id: number, updateUserDto: Partial<User>): Promise<User> {
+    if (updateUserDto.username) {
+      const isEmailValid = this.validateEmail(updateUserDto.username);
+      if (!isEmailValid) {
+        throw new BadRequestException('Invalid email format');
+      }
+      const isUsernameTaken = await this.isUsernameTaken(updateUserDto.username);
+      if (isUsernameTaken) {
+        throw new BadRequestException('Username is already taken');
+      }
+    }
+    if (updateUserDto.password) {
+      const hash = await bcrypt.hash(updateUserDto.password, 10);
+      updateUserDto.password = hash;
+    }
+    await this.usersRepository.update(id, updateUserDto);
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 }
