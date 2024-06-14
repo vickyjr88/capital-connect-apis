9dcc8f1e67b47d41e3e060ff96871545ee8f5561
Victor Karanja <vickyjr88@gmail.com>
2024-06-14 12:42:07 +0300

    Add description to sections and subsections.

--- src/subsection/dto/create-subsection.dto.ts
+++ src/subsection/dto/create-subsection.dto.ts
@@ -1,8 +1,13 @@
-import { IsNumber, MinLength } from "class-validator";
+import { IsNumber, IsOptional, IsString, MinLength } from "class-validator";
 
 export class CreateSubsectionDto {
     @MinLength(3)
+    @IsString()
     name: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
 
     @IsNumber()
     sectionId: number;
