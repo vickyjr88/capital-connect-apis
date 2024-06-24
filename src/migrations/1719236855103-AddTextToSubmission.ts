9f087c2463eeb959d13754d8ea3d799819a7e356
christhiaine <chris.thiaine@wasoko.com>
2024-06-24 16:52:11 +0300

    text not null migration fix

--- src/migrations/1719236855103-AddTextToSubmission.ts
+++ src/migrations/1719236855103-AddTextToSubmission.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddTextToSubmission1719234014525 implements MigrationInterface {
-    name = 'AddTextToSubmission1719234014525'
+export class AddTextToSubmission1719236855103 implements MigrationInterface {
+    name = 'AddTextToSubmission1719236855103'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "submission" ADD "text" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "submission" ADD "text" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
