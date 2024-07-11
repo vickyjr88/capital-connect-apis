aaabbb218de114949fefa3cdf85aa91ee7ef7eac
Victor Karanja <vickyjr88@gmail.com>
2024-07-11 17:27:07 +0300

    Add endpoint to fetch scoring based on score and type.

--- src/scoring/scoring.module.ts
+++ src/scoring/scoring.module.ts
@@ -3,10 +3,17 @@ import { ScoringService } from './scoring.service';
 import { ScoringController } from './scoring.controller';
 import { Scoring } from './entities/scoring.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomLogger } from 'src/shared/utils/custom-logger.util';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Scoring])],
   controllers: [ScoringController],
-  providers: [ScoringService],
+  providers: [
+    ScoringService,
+    CustomLogger,
+    {
+      provide: 'CustomLogger',
+      useClass: CustomLogger,
+    },],
 })
 export class ScoringModule {}
