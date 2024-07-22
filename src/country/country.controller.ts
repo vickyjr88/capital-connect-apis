53eeef1816f243a61b0331f0a0c21eff6c2c8310
ian irungu <gatumaian@gmail.com>
2024-07-22 17:07:17 +0300

    created and implemented rest apis for post, get, update and delete countries

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Param, Put, NotFoundException, BadRequestException } from '@nestjs/common';
 import { CountryService } from './country.service';
 import { Country } from './entities/country.entity';
 import { CreateCountryDto } from './dto/create-country.dto';
 import * as countriesData from './countries.json' // Create a JSON file with country data
+import { UpdateCountryDto } from './dto/update-country.dto';
+import throwInternalServer from 'src/shared/utils/exceptions.util';
 
 @Controller('countries')
 export class CountryController {
@@ -26,5 +28,29 @@ export class CountryController {
     }));
   
     return await this.countriesService.bulkCreate(countries);
+  }
+
+  @Get(':id')
+    findOne(@Param('id') id: number): Promise<Country> {
+        return this.countriesService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateCountryDto: UpdateCountryDto) {
+    try {
+      await this.countriesService.findOne(+id);
+      const country = await this.countriesService.update(+id, updateCountryDto);
+      return country;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new BadRequestException(`Country with id ${id} not found`);
+      }
+      throwInternalServer(error)
+    }
+  }
+
+  @Delete(':id')
+    remove(@Param('id') id: number): Promise<void> {
+        return this.countriesService.remove(id);
   }
 }
--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Country } from './entities/country.entity';
 import { CreateCountryDto } from './dto/create-country.dto';
+import { UpdateCountryDto } from './dto/update-country.dto';
 
 @Injectable()
 export class CountryService {
@@ -21,5 +22,26 @@ export class CountryService {
 
   async bulkCreate(countries: CreateCountryDto[]): Promise<Country[]> {
     return this.countriesRepository.save(countries);
+  }
+
+  async findOne(id: number) {
+    const country = await this.countriesRepository.findOneBy({ id });
+    if (!country) {
+      throw new NotFoundException(`Country with id ${id} not found`);
+    } 
+    return country;
+  }
+
+  async update(id: number, updateCountryDto: UpdateCountryDto) {
+    const { name, code } = updateCountryDto;
+    const updates = {};
+    if (name) updates['name'] = name;
+    if (code) updates['code'] = code;
+    if (Object.keys(updates).length > 0) await this.countriesRepository.update(id, updateCountryDto);
+    return this.countriesRepository.findOneBy({ id });
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.countriesRepository.delete(id);
   }
 }
