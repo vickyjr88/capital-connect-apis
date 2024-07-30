01dff69796b8cdee7de51fd816374bf72c1aee23
Victor Karanja <vickyjr88@gmail.com>
2024-07-30 22:52:27 +0300

    Fix Pesapal integration.

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Delete, Get, HttpException, Param, Post, Put, Query, Req, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { BookingService } from "./booking.service";
-import { PaymentService } from "src/payment/payment.service";
-import { CreateBookingDto } from "./dto/create-booking.dto";
-import { UpdateBookingDto } from "./dto/update-booking.dto";
-import { HeadersToken } from "src/shared/headers.decorators";
-import { HttpService } from "@nestjs/axios";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { BookingService } from './booking.service';
+import { PaymentService } from 'src/payment/payment.service';
+import { CreateBookingDto } from './dto/create-booking.dto';
+import { UpdateBookingDto } from './dto/update-booking.dto';
+import { HeadersToken } from 'src/shared/headers.decorators';
+import { HttpService } from '@nestjs/axios';
 
 @Controller('bookings')
 @UseGuards(JwtAuthGuard)
@@ -14,70 +25,97 @@ export class BookingController {
   constructor(
     private readonly bookingService: BookingService,
     private readonly paymentService: PaymentService,
-    private readonly httpService: HttpService
+    private readonly httpService: HttpService,
   ) {}
 
   @Post()
-  async createBooking(@HeadersToken() pesapalToken: string, @Body() createBookingDto: CreateBookingDto, @Req() req) {
+  async createBooking(
+    @HeadersToken() pesapalToken: string,
+    @Body() createBookingDto: CreateBookingDto,
+    @Req() req,
+  ) {
     const { calendlyEventId } = createBookingDto;
     const user = req.user;
-    const booking = await this.bookingService.createBooking(calendlyEventId, user.id);
+    const booking = await this.bookingService.createBooking(
+      calendlyEventId,
+      user.id,
+    );
 
-    console.log("User", user);
+    console.log('User', user);
 
     const bookingResponse = {} as any;
     bookingResponse.bookingId = booking.id;
 
     try {
-      const response = await this.httpService.post(`${process.env.PESAPAL_BASE_URL}/Transactions/SubmitOrderRequest`, {
-        "id": booking.id,
-        "currency": "KES",
-        "amount": process.env.ADVISORY_SESSIONS_COST,
-        "description": "Advisory session booking fee.",
-        "callback_url": process.env.PESAPAL_CALLBACK_URL,
-        "redirect_mode": "TOP_WINDOW",
-        "notification_id": process.env.PESAPAL_NOTIFICATION_ID,
-        "branch": "Capital Connect Africa App",
-        "billing_address": {
-            "email_address": user.username,
-            "phone_number": "", // ToDo: Get user phone number
-            "country_code": "KE",
-            "first_name": user.firstName,
-            "middle_name": user.lastName,
-            "last_name": user.lastName,
-            "line_1": "Rasin Capital",
-            "line_2": "",
-            "city": "",
-            "state": "",
-            "postal_code": "",
-            "zip_code": ""
-        }
-      }, 
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${pesapalToken}`
-        }
-      }).toPromise();
+      const response = await this.httpService
+        .post(
+          `${process.env.PESAPAL_BASE_URL}/Transactions/SubmitOrderRequest`,
+          {
+            id: booking.id,
+            currency: 'KES',
+            amount: process.env.ADVISORY_SESSIONS_COST,
+            description: 'Advisory session booking fee.',
+            callback_url: process.env.PESAPAL_CALLBACK_URL,
+            redirect_mode: 'TOP_WINDOW',
+            notification_id: process.env.PESAPAL_NOTIFICATION_ID,
+            branch: 'Capital Connect Africa App',
+            billing_address: {
+              email_address: user.username,
+              phone_number: '', // ToDo: Get user phone number
+              country_code: 'KE',
+              first_name: user.firstName,
+              middle_name: user.lastName,
+              last_name: user.lastName,
+              line_1: 'Rasin Capital',
+              line_2: '',
+              city: '',
+              state: '',
+              postal_code: '',
+              zip_code: '',
+            },
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${pesapalToken}`,
+            },
+          },
+        )
+        .toPromise();
 
       // Redirect url: https://pay.pesapal.com/iframe/PesapalIframe3/Index?OrderTrackingId=[order_tracking_id]
       const res = response.data;
-      console.log("Response", res);
-      if (res.status !== '200') throw new HttpException("Failed to initiate payment", 500);
+      // console.log('Response', res);
+      if (res.status !== '200')
+        throw new HttpException(
+          `Failed to initiate payment ${res.message}`,
+          500,
+        );
       bookingResponse.orderTrackingId = res.order_tracking_id;
       bookingResponse.redirectUrl = res.redirect_url;
-      const payment = await this.paymentService.createPayment({ bookingId: booking.id, orderTrackingId: res.order_tracking_id, userId: user.id });
+      const payment = await this.paymentService.createPayment({
+        bookingId: booking.id,
+        orderTrackingId: res.order_tracking_id,
+        userId: user.id,
+      });
       bookingResponse.paymentId = payment.id;
     } catch (error) {
-      console.error("Error", error);
-      throw new HttpException("Failed to initiate payment", 500);
+      console.error('Error', error);
+      throw new HttpException(
+        `Failed to initiate payment ${error.message}`,
+        500,
+      );
     }
 
     return bookingResponse;
   }
 
   @Get()
-  findAll(@Req() req, @Query('page') page: number = 1, @Query('count') limit: number = 10) {
+  findAll(
+    @Req() req,
+    @Query('page') page: number = 1,
+    @Query('count') limit: number = 10,
+  ) {
     const user = req.user;
     return this.bookingService.findAll(user, page, limit);
   }
