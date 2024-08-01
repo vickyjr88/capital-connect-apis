cea6a7ac1806ec7ba4041751fc62a67258939e83
Victor Karanja <vickyjr88@gmail.com>
2024-08-01 12:10:04 +0300

    Create new send SMS implementation.

--- src/mobile/otp.service.ts
+++ src/mobile/otp.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+const SibApiV3Sdk = require('sib-api-v3-typescript');
 
 @Injectable()
 export class OtpService {
@@ -7,6 +8,26 @@ export class OtpService {
   private readonly apiUrl = 'http://api.brevo.com/v3/transactionalSMS/sms';
 
   async sendSms(mobileNumber: string, message: string): Promise<any> {
+    let apiInstance = new SibApiV3Sdk.TransactionalSMSApi()
+
+    let apiKey = apiInstance.authentications['apiKey'];
+    apiKey.apiKey = process.env.BREVO_API_KEY;
+
+    let sendTransacSms = new SibApiV3Sdk.SendTransacSms();
+    sendTransacSms.sender = 'CapitalCon';
+    sendTransacSms.recipient = mobileNumber;
+    sendTransacSms.content = message;
+    sendTransacSms.type = 'transactional';
+    sendTransacSms.webUrl = 'https://example.com/notifyUrl';
+
+    apiInstance.sendTransacSms(sendTransacSms).then(function(data) {
+      console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+    }, function(error) {
+      console.error(error);
+    });
+  }
+
+  async sendSmsOld(mobileNumber: string, message: string): Promise<any> {
     try {
       const response = await axios.post(
         this.apiUrl,
