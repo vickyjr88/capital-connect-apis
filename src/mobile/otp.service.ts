fc0bb550e290523a8e6fec30b6bf629f45da7269
Victor Karanja <vickyjr88@gmail.com>
2024-08-08 15:03:49 +0300

    Minor fix.

--- src/mobile/otp.service.ts
+++ src/mobile/otp.service.ts
@@ -4,27 +4,32 @@ const SibApiV3Sdk = require('sib-api-v3-typescript');
 
 @Injectable()
 export class OtpService {
-  private readonly apiKey = process.env.BREVO_API_KEY;
+  private readonly apiKey = process.env.BREVO_API_KEY_SMS;
   private readonly apiUrl = 'http://api.brevo.com/v3/transactionalSMS/sms';
 
   async sendSms(mobileNumber: string, message: string): Promise<any> {
-    let apiInstance = new SibApiV3Sdk.TransactionalSMSApi()
+    const apiInstance = new SibApiV3Sdk.TransactionalSMSApi();
 
-    let apiKey = apiInstance.authentications['apiKey'];
-    apiKey.apiKey = process.env.BREVO_API_KEY;
+    const apiKey = apiInstance.authentications['apiKey'];
+    apiKey.apiKey = process.env.BREVO_API_KEY_SMS;
 
-    let sendTransacSms = new SibApiV3Sdk.SendTransacSms();
+    const sendTransacSms = new SibApiV3Sdk.SendTransacSms();
     sendTransacSms.sender = 'CapitalCon';
     sendTransacSms.recipient = mobileNumber;
     sendTransacSms.content = message;
     sendTransacSms.type = 'transactional';
     sendTransacSms.webUrl = 'https://example.com/notifyUrl';
 
-    apiInstance.sendTransacSms(sendTransacSms).then(function(data) {
-      console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-    }, function(error) {
-      console.error(error);
-    });
+    apiInstance.sendTransacSms(sendTransacSms).then(
+      function (data) {
+        console.log(
+          'API called successfully. Returned data: ' + JSON.stringify(data),
+        );
+      },
+      function (error) {
+        console.error(error);
+      },
+    );
   }
 
   async sendSmsOld(mobileNumber: string, message: string): Promise<any> {
